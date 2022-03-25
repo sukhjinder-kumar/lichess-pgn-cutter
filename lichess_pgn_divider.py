54e039d15342933b075ab70fb20f7c4f11a17649
sukhjinder-kumar <sukhjinderkumar007101@gmail.com>
2022-03-25 16:50:00 +0530

    Updated pgn cutter

--- lichess_pgn_divider.py
+++ lichess_pgn_divider.py
@@ -1,7 +1,15 @@
-# write the file name to break here, no .txt please
-path = "Garry_Kasparov_-_My_Great_Predecessors_Vol_1"
-x = 30  # this is the limit on size of pgn
+import os # for making directory
 
+# write the file name to break here, no .txt please
+path = "Mastering_Endgame_Strategy_Everyman_2013_Hellsten_Johan1"
+x = 30 
+
+# make new directory by the name of path
+cur_dir = os.getcwd()
+path_ = os.path.join(cur_dir, path)
+os.mkdir(path_)
+
+# main code 
 path1 = path + ".pgn"
 f = open(path1,'r')
 pgn = f.read() 
@@ -19,8 +27,8 @@ for line in f.readlines():
             # print(variations)
             num_file = num_file + 1 
 # line[-2:] == "*\n"
-# print(variations)
-# print(num_file) 
+print(variations)
+print(num_file) 
 
 f.seek(0)
 
@@ -29,7 +37,8 @@ f.seek(0)
 base_name = path + "_"
 list_name = []
 for i in range (1,num_file+1):
-    dummy = base_name + str(i) + ".pgn"
+    dummy_ = base_name + str(i) + ".pgn"
+    dummy = os.path.join(path_,dummy_)
     list_name.append(dummy)
 
 f2 = open(list_name[0], 'w')
